0b99a3fc5ed799b878bd881893a8051709c4c299
Ryan Schenk <rschenk@gmail.com>
2024-04-23 08:32:45 -0400

    Custom plugin: indent line with custom config

---
+++ lua/custom/plugins/indent_line.lua
@@ -0,0 +1,14 @@
+return {
+  { -- Add indentation guides even on blank lines
+    'lukas-reineke/indent-blankline.nvim',
+    -- Enable `lukas-reineke/indent-blankline.nvim`
+    -- See `:help ibl`
+    main = 'ibl',
+    opts = {
+      scope = {
+        -- Do not show an underline on the first line
+        show_start = false,
+      },
+    },
+  },
+}
